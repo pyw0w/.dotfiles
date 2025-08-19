88c7b450ecc4a7fcc9efa304e830ba88d3c16a84
pyw0w <myxi2002@gmail.com>
2025-08-19 21:20:50 +0500

    modules(fabric): сделать modules/fabric/config/main.py главным входом; обёртка fabric-run и корректные GI/GSettings

--- modules/fabric/default.nix
+++ modules/fabric/default.nix
@@ -2,6 +2,11 @@
 args@{ config, lib, pkgs, variables, ... }:
 let
   fabricEnv = pkgs.python3.withPackages (ps: [ pkgs.local.fabric ]);
+  fabricRunner = pkgs.writeShellScriptBin "fabric-run" ''
+    export GI_TYPELIB_PATH=${pkgs.gtk3}/lib/girepository-1.0:${pkgs.gdk-pixbuf}/lib/girepository-1.0:${pkgs.gobject-introspection}/lib/girepository-1.0:${pkgs.libdbusmenu-gtk3}/lib/girepository-1.0:${pkgs.gtk-layer-shell}/lib/girepository-1.0:$GI_TYPELIB_PATH
+    export GSETTINGS_SCHEMA_DIR=${pkgs.gsettings-desktop-schemas}/share/glib-2.0/schemas:${pkgs.gtk3}/share/glib-2.0/schemas:$GSETTINGS_SCHEMA_DIR
+    exec ${fabricEnv}/bin/python %h/.config/fabric/main.py
+  '';
 in
 lib.mkModule "fabric" config {
   environment.systemPackages = [
@@ -22,7 +27,7 @@ lib.mkModule "fabric" config {
         PartOf = [ "graphical-session.target" ];
       };
       Service = {
-        ExecStart = "${fabricEnv}/bin/python -m fabric";
+        ExecStart = "${fabricRunner}/bin/fabric-run";
         Restart = "on-failure";
         RestartSec = 2;
         Environment = [ "PYTHONUNBUFFERED=1" ];
